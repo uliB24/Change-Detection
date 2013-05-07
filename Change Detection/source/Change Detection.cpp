2d035cde9b2f665770819744e80a315c30535af4
unknown <ulrich.berger1@gmail.com>
2013-05-07 14:20:53 +0200

    Klasse fuer Background

--- Change Detection/source/Change Detection.cpp
+++ Change Detection/source/Change Detection.cpp
@@ -7,10 +7,11 @@
 #include <sstream>
 #include <iomanip> 
 
-#include "test.h"
 
 #include <opencv2/opencv.hpp>
-#include "opencv2/gpu/gpu.hpp"
+#include <opencv2/gpu/gpu.hpp>
+
+#include "Background.h"
 
 #ifdef HAVE_OPENCV_NONFREE
 #include "opencv2/nonfree/gpu.hpp"
@@ -25,7 +26,7 @@ namespace
 {
   //Konstanten:
   //string path = "C:/Users/Uli/Documents/Uni/Semester 6/E_trainingsdaten/trainingsdaten/dynamicbackground/canoe/input/in"; // 4 sigma, 1x1 erode
-  string path = "C:/Users/Uli/Documents/Uni/Semester 6/E_trainingsdaten/trainingsdaten/dynamicbackground/fountain/input/in"; // 6 sigma + 3x3 erode
+  string path = "E:/uniSonstiges/E_trainingsdaten/trainingsdaten/dynamicbackground/fountain/input/in"; // 6 sigma + 3x3 erode
   //string path = "C:/Users/Uli/Documents/Uni/Semester 6/E_trainingsdaten/trainingsdaten/base/highway/input/in"; // klappt nicht gut, da autos schon am Anfang da lang fahren
   // -> bei der erstellung der Varianz muss bereits erkannt werden, dass fahrende Autos nicht in die Varianz und den Hintergrund einflieﬂen
   //string path = "C:/Users/Uli/Documents/Uni/Semester 6/E_trainingsdaten/trainingsdaten/shadow/bungalows/input/in"; // schtatten muss entfernt werden, autos fahren schon am begin da lang(nicht so schlimm wie bei highway)
@@ -57,172 +58,53 @@ namespace
 int start(const char* path);
 int main(int argc, char *argv[])
 {
-  //return start("C:/Users/Uli/Documents/Uni/Semester 6/E_trainingsdaten/trainingsdaten/dynamicbackground/fountain/input/in000001.jpg");
-  //testGPUBackgroundSub(Method::FGD_STAT);
+  Background bg(path,2,2,4);
 
-  //return 0;
-  /*
-  float x[15] = {1,4,9,5,3,4,5,4,6,7,4,4,6,3,5};
-  float n = 0;
-  float m = 0;
-  float m2 = 0;
-  float v = 0;
-  for(int i = 0; i < 15; ++i)
+  Mat frame;
+  Mat back;
+  Mat fore;
+
+  std::vector<std::vector<Point> > contours;
+
+  namedWindow("Frame");
+  namedWindow("Background");
+  namedWindow("fore");
+  namedWindow("forevor");
+  namedWindow("forenach");
+
+  namedWindow("mean");
+  namedWindow("var");
+
+  namedWindow("change");
+
+  size_t count = 0;
+
+  while(true)
   {
-    ++n;
-    float delta = x[i] - m;
-    m += delta / n; 
-    m2 += delta * (x[i] - m);
-    if(n>1)
-      v = m2 / (n-1);
-    cout << v << endl;
+    ++count;
+    ostringstream oss;
+    oss << path;
+    oss << std::setfill ('0') << std::setw (6);
+    oss << count << ".jpg";
+
+    Mat frame = imread(oss.str(), CV_LOAD_IMAGE_COLOR);
+    if(!frame.data)
+      return false;
+
+    bg.update(frame, fore);
+
+    imshow("fore",fore);
+
+    bg.getBackgroundImage(back);
+
+    findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
+    drawContours(frame,contours,-1,Scalar(0,0,255),2);
+
+    imshow("Frame",frame);
+    imshow("Background",back);
+    if(waitKey(10) >= 0) break;
   }
   return 0;
-  */
-
-
-    Mat frame;
-    Mat back;
-    Mat fore;
-    //VideoCapture cap(0);
-    BackgroundSubtractorMOG2 bg;      
-    int size2 = 2;
-    Mat erosion = getStructuringElement( MORPH_ELLIPSE,
-                                       Size( 2*size2 + 1, 2*size2+1 ),
-                                       Point( size2, size2 ) );
-    Mat dilatation = getStructuringElement( MORPH_ELLIPSE,
-                                       Size( 2*size2 + 1, 2*size2+1 ),
-                                       Point(size2, size2 ) );
-
-
-    //bg.nmixtures = 3;
-    //bg.bShadowDetection = false;
- 
-    std::vector<std::vector<Point> > contours;
- 
-    namedWindow("Frame");
-    namedWindow("Background");
-    namedWindow("fore");
-    namedWindow("forevor");
-    namedWindow("forenach");
-
-    namedWindow("mean");
-    namedWindow("var");
- 
-	  size_t count = 1;
-    ostringstream oss2;
-		  oss2 << path;
-		  oss2 << std::setfill ('0') << std::setw (6);
-		  oss2 << count << ".jpg";
-    frame = imread(oss2.str(), CV_LOAD_IMAGE_COLOR);
-    
-    Mat mean(frame.size(), CV_32FC3, Scalar(0.0f,0.0f,0.0f));
-    Mat M2(frame.size(), CV_32FC3, Scalar(0.0f,0.0f,0.0f));
-    Mat var(frame.size(), CV_32FC3, Scalar(0.0f,0.0f,0.0f));
-
-    float n = 0;
-
-    Vec3f delta;
-    Size size = frame.size();
-    for(;;)
-    {
-      
-		  ostringstream oss;
-		  oss << path;
-		  oss << std::setfill ('0') << std::setw (6);
-		  oss << count << ".jpg";
-		  frame = imread(oss.str(), CV_LOAD_IMAGE_COLOR);
-      //frame.convertTo(frame, CV_32FC3);
-		  ++count;
-		  if(!frame.data)
-			  break;
-        //cap >> frame;
-      ++n;
-      /*
-      http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
-      online_variance
-      */
-
-      if(n < 200)
-      {
-      for(int x = 0; x < size.width; ++x)
-      {
-        for(int y = 0; y < size.height; ++y)
-        {
-          
-          for(int i = 0; i < 3; ++i)
-          {
-            float f = frame.at<Vec3b>(y, x).val[i];
-            float me = mean.at<Vec3f>(y, x).val[i];
-            delta.val[i] = frame.at<Vec3b>(y, x).val[i] - mean.at<Vec3f>(y, x).val[i];
-            mean.at<Vec3f>(y, x).val[i] += delta.val[i] / n;
-            M2.at<Vec3f>(y,x).val[i] += delta.val[i] * (frame.at<Vec3b>(y, x).val[i] - mean.at<Vec3f>(y, x).val[i]);
-            if(n>198)
-              var.at<Vec3f>(y,x).val[i] = sqrt(M2.at<Vec3f>(y,x).val[i] / (n-1));
-            //if(x == 60 && y == 120)
-              //cout << var.at<Vec3f>(y,x).val[i] << endl;
-          }
-        }
-      }
-      }
-
-      
-
-      /*
-      http://de.wikipedia.org/wiki/Standardabweichung#Berechnung_f.C3.BCr_auflaufende_Messwerte
-      
-      for(int x = 0; x < size.width; ++x)
-      {
-        for(int y = 0; y < size.height; ++y)
-        {
-          mean.at<Vec3b>(y, x) += frame.at<Vec3b>(y, x);
-          for(int i = 0; i < 3; ++i)
-          {
-            M2.at<Vec3b>(y,x).val[i] += frame.at<Vec3b>(y, x).val[i] * frame.at<Vec3b>(y, x).val[i];
-            if(n>1)
-              var.at<Vec3b>(y,x).val[i] = sqrt((1/(n-1)) * ( M2.at<Vec3b>(y,x).val[i] - (1/n) * mean.at<Vec3b>(y, x).val[i] * mean.at<Vec3b>(y, x).val[i]));
-          }
-        }
-      }
-      */
-      imshow("mean",mean / 255);
-      imshow("var",var / 255);
-
-
-      bg.operator ()(frame,fore);
-      imshow("forevor",fore);
-
-      bg.getBackgroundImage(back);
-      for(size_t x = 0; x < fore.size().width; ++x)
-      {
-        for(size_t y = 0; y < fore.size().height; ++y)
-        {
-          //for(int i = 0; i < 3; ++i)
-            //if(x == 110 && y == 145)
-              //cout << "Diff: " << abs(frame.at<Vec3b>(y,x).val[i] - back.at<Vec3b>(y,x).val[i]) << endl << "var: " << var.at<Vec3f>(y,x).val[i] << "\n";
-          //cout << endl;
-          if(abs(frame.at<Vec3b>(y,x).val[0] - back.at<Vec3b>(y,x).val[0]) < 6*(var.at<Vec3f>(y,x).val[0]) ||
-             abs(frame.at<Vec3b>(y,x).val[1] - back.at<Vec3b>(y,x).val[1]) < 6*(var.at<Vec3f>(y,x).val[1]) ||
-             abs(frame.at<Vec3b>(y,x).val[2] - back.at<Vec3b>(y,x).val[2]) < 6*(var.at<Vec3f>(y,x).val[2]))
-          {
-            fore.at<uchar>(y,x) = 0;
-          }
-        }
-      }
-      imshow("forenach",fore);
-
-
-      erode(fore,fore,erosion);
-      dilate(fore,fore,dilatation);
-      imshow("fore",fore);
-      findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-      drawContours(frame,contours,-1,Scalar(0,0,255),2);
-      
-      imshow("Frame",frame);
-      imshow("Background",back);
-      if(waitKey(10) >= 0) break;
-    }
-    return 0;
 }
 
 namespace
